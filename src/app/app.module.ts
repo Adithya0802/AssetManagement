c008bfc07952a6a60a436a6c5c9ea17399b26bf1
ananth61 <ananth.murugan16@gmail.com>
2022-09-14 17:20:55 +0530

    emp details changes added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeedetailsComponent } from './employeedetails/employeedetails.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeedetailsComponent
   ],
   imports: [
     BrowserModule,
