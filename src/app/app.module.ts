d6e5f39453590a83abc5ab8e6af209d73d7a3a1f
sampathkumar-samiappan <samtensampath97@gmail.com>
2022-09-14 16:56:24 +0530

    modulechange

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AssetloginComponent } from './assetlogin/assetlogin.component';
 import { FooterComponent } from './footer/footer.component';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatListModule } from '@angular/material/list';
-import {InputTextModule} from 'primeng/inputtext';
 import { HeaderComponent } from './header/header.component';
-import {PasswordModule} from 'primeng/password';
-import {DropdownModule} from 'primeng/dropdown';
 
 @NgModule({
   declarations: [
@@ -25,15 +17,6 @@ import {DropdownModule} from 'primeng/dropdown';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatIconModule,
-    MatSidenavModule,
-    MatToolbarModule,
-    MatMenuModule,
-    MatListModule,
-    InputTextModule,
-    PasswordModule,
-    DropdownModule
-   
   ],
   providers: [],
   bootstrap: [AppComponent]
